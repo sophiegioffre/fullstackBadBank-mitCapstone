f3e8e7ad122d3051ed6984efd2ad1a27f5209f42
Sophie <sophie.gioff@gmail.com>
2022-10-01 13:54:20 -0400

    Adds Logout / Updates Navbar

--- public/navbar.js
+++ public/navbar.js
@@ -1,4 +1,6 @@
 function NavBar(){
+  const [show, setShow] = React.useState(true);
+
   return(
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -7,26 +9,37 @@ function NavBar(){
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/login/">Login</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/deposit/">Deposit</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/withdraw/">Withdraw</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/balance/">Balance</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#/alldata/">AllData</a>
-          </li>          
-        </ul>
+        {show ? (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link">Hi, Name!</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/deposit/">Deposit</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/withdraw/">Withdraw</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/balance/">Balance</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/alldata/">AllData</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/" onClick={()=> setShow(false)} >Logout</a>
+            </li>        
+          </ul>
+        ) : (
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#/login/">Login</a>
+            </li>         
+          </ul>
+        )}
       </div>
     </nav>
 
