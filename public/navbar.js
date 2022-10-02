43ed3e2a68f58500832deadd33af12142ce60079
Sophie <sophie.gioff@gmail.com>
2022-10-02 13:31:12 -0400

    Revert to basic navbar

--- public/navbar.js
+++ public/navbar.js
@@ -1,5 +1,4 @@
 function NavBar(){
-  const [show, setShow] = React.useState(true);
 
   return(
 
@@ -9,37 +8,26 @@ function NavBar(){
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        {show ? (
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link">Hi, Name!</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/deposit/">Deposit</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/withdraw/">Withdraw</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/balance/">Balance</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/alldata/">AllData</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/" onClick={()=> setShow(false)} >Logout</a>
-            </li>        
-          </ul>
-        ) : (
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#/CreateAccount/">Create Account</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#/login/">Login</a>
-            </li>         
-          </ul>
-        )}
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#/CreateAccount/">Create Account</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/login/">Login</a>
+          </li> 
+          <li className="nav-item">
+            <a className="nav-link" href="#/deposit/">Deposit</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/withdraw/">Withdraw</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/balance/">Balance</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#/alldata/">AllData</a>
+          </li>      
+        </ul>
       </div>
     </nav>
 
