1a2909235d82cbc2f35163a7a28c8e8e50ecec4c
Sophie <sophie.gioff@gmail.com>
2022-10-03 17:20:50 -0400

    Updates styles

--- public/context.js
+++ public/context.js
@@ -11,14 +11,16 @@ function Card(props){
   }
 
   return (
-    <div className={classes()} style={{maxWidth: "18rem"}}>
-      <div className="card-header">{props.header}</div>
-      <div className="card-body">
-        {props.title && (<h5 className="card-title">{props.title}</h5>)}
-        {props.text && (<p className="card-text">{props.text}</p>)}
-        {props.body}
-        {props.status && (<div id='createStatus'>{props.status}</div>)}
-      </div>
-    </div>      
+    <div className="container" style={{padding: "20px", paddingTop: "50px"}}>
+      <div className={classes()}>
+        <div className="card-header">{props.header}</div>
+        <div className="card-body">
+          {props.title && (<h5 className="card-title">{props.title}</h5>)}
+          {props.text && (<p className="card-text">{props.text}</p>)}
+          {props.body}
+          {props.status && (<div id='createStatus'>{props.status}</div>)}
+        </div>
+      </div> 
+    </div>
   );    
 }
