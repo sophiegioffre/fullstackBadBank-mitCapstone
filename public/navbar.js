6e57803c811f1ffacf2acb15e8a71f7a842bd4a9
Sophie <sophie.gioff@gmail.com>
2022-10-04 15:46:49 -0400

    Adds functionality to navbar collapse

--- public/navbar.js
+++ public/navbar.js
@@ -4,8 +4,8 @@ function NavBar(){
 
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="#">BadBank</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
