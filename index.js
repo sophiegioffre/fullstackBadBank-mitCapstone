ec2f2638325a6182f6d451e32a9fe928bf6d8816
Sophie <sophie.gioff@gmail.com>
2022-10-04 20:40:58 -0400

    Updates port

--- index.js
+++ index.js
@@ -99,4 +99,4 @@ app.get('/account/all', function (req, res) {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
